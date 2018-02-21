4abbfb0714ccd04749abc98413d7c92290de0b54
Jahnavidk <Jahnavidk261@gmail.com>
2018-02-21 14:01:45 -0500

    Loading Images

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Ranganathan, Ravi (Contractor) on 2/21/18.
+//  Copyright © 2018 Comcast. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = selectedImage
+        if let image = selectedImage {
+            imageView.image = UIImage(named: image)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
